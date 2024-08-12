5c1e1b27c91bf8cfbf0bae5115fd30b98407a90f
Hoai <hoai.nguyen25@student.passerellesnumeriques.org>
2024-08-12 11:07:08 +0700

    Completed task coding API for Service Page

--- PHP/routes/api.php
+++ PHP/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ServiceController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Users
+Route::get('services', [ServiceController::class, 'viewAllServices']);
