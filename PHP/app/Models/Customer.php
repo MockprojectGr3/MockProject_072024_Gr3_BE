8d2b70cd7f09eb829d01ca5fa5900b77ce5a8356
NguyenNgan03 <142873464+NguyenNgan03@users.noreply.github.com>
2024-08-27 09:02:06 +0700

    Feat/crud customer (#29)
    * feat/feedbackService
    
    * feat/crudcustomer
    
    ---------
    
    Co-authored-by: Tran Duc Phat <144576717+tranducphat204@users.noreply.github.com>

--- PHP/app/Models/Customer.php
+++ PHP/app/Models/Customer.php
@@ -10,11 +10,18 @@ class Customer extends Model
 {
     use HasFactory;
 
-    protected $table = 'customers';
-
+    protected $table = 'Customer';
+    protected $primaryKey = 'id';
     protected $fillable = [
         'user_id',
         'avatar',
+        'bio',
+        'deleted_at'
+    ];
+
+    protected $dates = ['deleted_at'];
+
+    public $timestamps = false;
         'bio'
     ];
 
--- PHP/routes/api.php
+++ PHP/routes/api.php
@@ -1,10 +1,10 @@
 <?php
 
 use App\Http\Controllers\ContactUsController;
-use App\Http\Controllers\CustomerControllers;
 use App\Http\Controllers\EquipmentController;
 use App\Http\Controllers\ServiceController;
 use App\Http\Controllers\FeedbackServiceController;
+use App\Http\Controllers\CustomerController;
 use App\Models\ContactUs;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -34,6 +34,13 @@ Route::prefix('feedback-services')->group(function(){
     Route::post('/', [FeedbackServiceController::class, 'store']);
 });
 
+Route::prefix('customers')->group(function(){
+    Route::get('/', [CustomerController::class, 'index']);
+    Route::post('/', [CustomerController::class, 'store']);
+    Route::get('/{id}', [CustomerController::class, 'show']);
+    Route::put('/{id}', [CustomerController::class, 'update']);
+    Route::delete('/{id}', [CustomerController::class, 'destroy']);
+});
 // Users
 Route::get('users/services', [ServiceController::class, 'viewAllServices']);
 Route::get('users/detail-service/{serviceId}', [ServiceController::class, 'viewDetailServices']);
